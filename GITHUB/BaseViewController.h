32b0b4d455b19004d0d9f16ce8f90ce038ec2a80
zhanghui <zhanghui@uzteam.com>
2016-09-17 09:14:21 +0800

    baseviewcontroller

---
+++ GITHUB/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  GITHUB
+//
+//  Created by 小飞鸟 on 16/9/17.
+//  Copyright © 2016年 小飞鸟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
